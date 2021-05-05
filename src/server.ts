2f757a3423d7f1deaa797c11d97d39c8aedfea59
Leandro-Faustino <leandrofaustino88@gmail.com>
2021-05-05 14:31:31 -0300

    update file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) =>
-  response.json({ message: "Hello my friends" })
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello my friends' }),
 );
 
-app.listen(3333, () => console.log("Server esta rodando!"));
+app.listen(3333, () => console.log('Server esta rodando!'));
