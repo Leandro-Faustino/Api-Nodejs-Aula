156683fedf55087c17a5e2d027069f7817f4dbc1
Leandro-Faustino <leandrofaustino88@gmail.com>
2021-05-05 14:08:59 -0300

    Setting config files nodejs and typescript

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) =>
+  response.json({ message: "Hello my friends" })
+);
+
+app.listen(3333, () => console.log("Server esta rodando!"));
